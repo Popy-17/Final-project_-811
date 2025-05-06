17b355e13eb7406447919f583b9328bf1db335fd
pd1156 <pd1156@ron.sr.unh.edu>
2025-05-06 11:03:43 -0400

    added the script to the dir

---
+++ script/basic_read_assessment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+    read_length=250
+    genome_size=7000000
+    for item in ./*_R1_001.fastq.gz;do
+    echo item:$(basename $item)
+    num_reads=$(zgrep -c "^@" $item)
+    echo num_reads:$num_reads
+    total_bases=$(($num_reads * read_length *2))
+    echo bases:$total_bases
+    coverage=$(($total_bases / genome_size))
+    echo coverage:$coverage
+    done

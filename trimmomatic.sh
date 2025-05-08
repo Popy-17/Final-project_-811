6e14b2930a1339fb1990c0b1823979df53d2e2ce
pd1156 <pd1156@ron.sr.unh.edu>
2025-05-07 20:35:52 -0400

    add the correct trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-source activate genomics
-mkdir -p trimmed-reads
-
-for R1 in ./*_R1_*.fastq.gz; do
-    base=$(basename "$R1" _R1_001.fastq.gz) # take help from "chatgpt" during this step"
-    R2="${R1/_R1_001.fastq.gz/_R2_001.fastq.gz}" 
-    trim_scriptV2.sh "$R1" "$R2"
-   done
-
+    source activate genomics
+      R1=$1
+      R2=$2
+   ADAPTERS="/home/share/anaconda/envs/genomics/share/trimmomatic-0.39-2/adapters/TruSeq3-PE.fa"
+   mkdir -p trimmed-reads/
+   o_for="$(basename $R1)"
+   o_rev="$(basename $R2)"
+   trimmomatic PE -threads 32 "$R1" "$R2" \
+  "trimmed-reads/$o_for" "trimmed-reads/unpaired-$o_for" \
+  "trimmed-reads/$o_rev" "trimmed-reads/unpaired-$o_rev" \
+  ILLUMINACLIP:"$ADAPTERS":2:30:10:8:true \
+  LEADING:3 TRAILING:3 SLIDINGWINDOW:4:10 MINLEN:36

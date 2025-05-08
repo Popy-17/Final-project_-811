b955c5bd7c9a946cbae5937018a983fa51fd6381
pd1156 <pd1156@ron.sr.unh.edu>
2025-05-07 20:39:40 -0400

    add this corrected genome_assembly_script.sh

--- genome_assembly_script.sh
+++ genome_assembly_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
   source activate genomics
-      outdir=spades_assembly_default
-      cat ./*unpaired*.fastq.gz > combined_unpaired.fastq.gz      
-      nohup spades.py -s combined_unpaired.fastq.gz -o $outdir -t 24 &
-
+  outdir=spades_assembly_default
+  nohup spades.py \
+  -1 ./02_S65_L001_R1_001.fastq.gz \
+  -2 ./02_S65_L001_R2_001.fastq.gz \
+  -1 ./03_S58_L001_R1_001.fastq.gz \
+  -2 ./03_S58_L001_R2_001.fastq.gz \
+  -s ./unpaired-02_S65_L001_R1_001.fastq.gz \
+  -s ./unpaired-02_S65_L001_R2_001.fastq.gz \
+  -s ./unpaired-03_S58_L001_R1_001.fastq.gz \
+  -s ./unpaired-03_S58_L001_R2_001.fastq.gz \
+  -o $outdir -t 24 &
